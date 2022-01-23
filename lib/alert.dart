2c5403b1917e07a7637d9291318b5f21fb98678f
Prakash Shekhar <prakashshkhr@gmail.com>
2022-01-23 04:02:59 -0500

    Add Alert ListView

--- lib/alert.dart
+++ lib/alert.dart
@@ -1,6 +1,8 @@
 class Alert{
-  String name;
-  int weight;
+  int action; //where 0 is mailbox opened, 1 is mailbox closed, 2 is object placed in mailbox, 3 is object removed from mailbox
+  String? weight;
   DateTime date;
-  Alert(this.name, this.weight, this.date);
+  Alert(this.action, double weight, this.date){
+    this.weight = weight.toString() + "kg";
+  }
 }
--- lib/alerts.dart
+++ lib/alerts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'alert.dart';
 
 class AlertsView extends StatefulWidget {
   const AlertsView({Key? key}) : super(key: key);
@@ -7,10 +8,54 @@ class AlertsView extends StatefulWidget {
 }
 
 class _AlertsViewState extends State<AlertsView> {
+  List<Alert> alertList = [];
+  void addAlert(Alert a){
+    alertList.insert(0, a);
+  }
+  void initState(){
+    addAlert(Alert(0, 0.0, DateTime.now()));
+    addAlert(Alert(2, 0.5, DateTime.now()));
+    addAlert(Alert(1, 0.5, DateTime.now()));
+  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("alerts"),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: alertList.length,
+        itemBuilder: (context, index){
+          String action;
+          Icon actionIc;
+          switch(alertList[index].action){
+            case 0: {
+              action = "Mailbox Opened";
+              actionIc = const Icon(Icons.lock_open);
+            } break;
+            case 1: {
+              action = "Mailbox Closed";
+              actionIc = const Icon(Icons.lock);
+            } break;
+            case 2: {
+              action = "Mail placed in Mailbox";
+              actionIc = const Icon(Icons.mark_email_unread);
+            } break;
+            case 3: {
+              action = "Mail removed from Mailbox";
+              actionIc = const Icon(Icons.mark_email_read);
+            } break;
+            default: {
+              action = "Error";
+              actionIc = const Icon(Icons.error);
+            }
+          }
+          return Card(child: ListTile(
+            leading: actionIc,
+            title: Text(action),
+            subtitle: Text(alertList[index].weight.toString()),
+            trailing: Text(alertList[index].date.toString()),
+          )
+          );
+        }
+      ),
     );
     }
 }
